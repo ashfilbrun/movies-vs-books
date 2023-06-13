fe562c06b08381c726e1f8991fde96ca1e487ed9
ashfilbrun <ashesclaire@gmail.com>
2023-06-13 14:21:07 -0700

    update user routers

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,6 @@
 // const express = require('express');
 // const Router = express.Router()
-// const controller = require('../controllers/reviewController.js')
+// const controller = require('../controllers/userController.js')
 
 // Router.get('/', controller.getUser)
 // Router.get('/:id', controller.getUserById)
